af12efe2e6a947e99c692aa3b09259bab00cdce7
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2022-03-27 23:42:27 -0300

    Updating and removing data

--- MoviesAPI/MoviesAPI/Controllers/MovieController.cs
+++ MoviesAPI/MoviesAPI/Controllers/MovieController.cs
@@ -38,4 +38,35 @@ public class MovieController : ControllerBase
             return Ok(movie);
         return NotFound();
     }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpdateMovie(Guid id, [FromBody] Movie newMovie)
+    {
+        var movie = _context.Movies.FirstOrDefault(movie => movie.Id == id);
+        
+        if (movie is null)
+            return NotFound();
+        
+        movie.Title = newMovie.Title;
+        movie.Genre = newMovie.Genre;
+        movie.Director = newMovie.Director;
+        movie.Duration = newMovie.Duration;
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteMovie(Guid id)
+    {
+        var movie =  _context.Movies.FirstOrDefault(movie => movie.Id == id);
+        
+        if (movie is null)
+            return NotFound();
+
+        _context.Remove(movie);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
 }
